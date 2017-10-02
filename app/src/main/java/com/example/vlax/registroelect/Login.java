d3e0ef4179ba01937c3677394d9247bcb7d03553
FELLSAT <willysaty1393@hotmail.com>
2017-10-02 14:52:53 -0500

    Inicio de sesion
    Crea e inicia

--- app/src/main/java/com/example/vlax/registroelect/Login.java
+++ app/src/main/java/com/example/vlax/registroelect/Login.java
@@ -1,10 +1,15 @@
 package com.example.vlax.registroelect;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
@@ -29,6 +34,17 @@ import java.util.Arrays;
  */
 
 public class Login extends AppCompatActivity {
+
+    EditText EditCorreo;
+    EditText EditContra;
+    Button BtnIngresar;
+
+    String correoElectronico;
+    String contrasena;
+
+    AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this,"Prueba",null,1);
+
+    /////////////////////////////////////////////////////////////////////////////
 
     private LoginButton loginButton;
     private CallbackManager callbackManager;
@@ -78,6 +94,51 @@ public class Login extends AppCompatActivity {
                 }
             }
         };
+
+        //////////////////////////////////////////////////////////////////////////////////
+        EditCorreo = (EditText) findViewById(R.id.usuario);
+        EditContra = (EditText) findViewById(R.id.clave);
+        BtnIngresar = (Button) findViewById(R.id.button2);
+
+        BtnIngresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                correoElectronico = EditCorreo.getText().toString();
+                contrasena = EditContra.getText().toString();
+
+                SQLiteDatabase bd = admin.getWritableDatabase();
+                Cursor c1 = bd.rawQuery("select count(*) from Registros where correo = '"+correoElectronico+"'", null);
+
+                if (c1.moveToFirst()) {
+                    // capturo la consulta
+                    int cantidad = Integer.parseInt(c1.getString(0));
+
+
+                    if(cantidad == 0){
+                        Intent intent = new Intent(Login.this, Registro.class);
+                        startActivity(intent);
+
+                        Toast.makeText(getApplicationContext(),"Usuario no registrado. LLene el formulario para crear un usuario", Toast.LENGTH_SHORT).show();
+                    }else{
+                        c1 = bd.rawQuery("select count(*) from Registros where correo = '"+correoElectronico+"' and contrasena='"+contrasena+"'", null);
+
+                        if (c1.moveToFirst()) {
+                            //
+                            int cantidad2 = Integer.parseInt(c1.getString(0));
+
+                            if(cantidad2 == 0){
+                                Toast.makeText(getApplicationContext(),"Contrasena incorrecta", Toast.LENGTH_SHORT).show();
+                            }else{
+                                Toast.makeText(getApplicationContext(),"Session iniciada", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                    }
+
+                }
+            }
+        });
+
     }
 
     private void handleFacebookAccessToken(AccessToken accessToken) {
