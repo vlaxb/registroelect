cb8c8e8c28dc2d92f9f899585d931403bde277cd
vlaxb <vladix88@gmail.com>
2017-10-10 17:27:06 -0500

    act
    act

--- app/src/main/java/com/example/vlax/registroelect/MainActivity.java
+++ app/src/main/java/com/example/vlax/registroelect/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.vlax.registroelect;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.facebook.login.LoginManager;
@@ -13,9 +15,11 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+
     private TextView nameTextView;
     private TextView emailTextView;
     private TextView uidTextView;
+    private TextView dialgobutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
         nameTextView = (TextView) findViewById(R.id.nameTextView);
         emailTextView = (TextView) findViewById(R.id.emailTextView);
         uidTextView = (TextView) findViewById(R.id.uidTextView);
+        dialgobutton = (Button) findViewById(R.id.dialogbutton);
 
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
@@ -40,6 +45,16 @@ public class MainActivity extends AppCompatActivity {
         } else {
             goLoginScreen();
         }
+
+        dialgobutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                Dialog dialog = new Dialog();
+                dialog.setContex(MainActivity.this);
+                dialog.show(fragmentManager,"ejemploDialogo");
+            }
+        });
     }
 
     private void goLoginScreen() {
---
+++ app/src/main/java/com/example/vlax/registroelect/RVAdapter.java
@@ -0,0 +1,62 @@
+package com.example.vlax.registroelect;
+
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by DESARROLLOTYT on 3/10/2017.
+ */
+
+public class RVAdapter extends RecyclerView.Adapter<RVAdapter.PersonViewHolder> {
+
+    public static class PersonViewHolder extends RecyclerView.ViewHolder {
+
+        CardView cv;
+        TextView personName;
+        TextView personAge;
+
+        PersonViewHolder(View itemView) {
+            super(itemView);
+            cv = (CardView)itemView.findViewById(R.id.cv);
+            personName = (TextView)itemView.findViewById(R.id.person_name);
+            personAge = (TextView)itemView.findViewById(R.id.person_age);
+
+        }
+    }
+
+    List<Person> persons;
+
+    RVAdapter(List<Person> persons){
+        this.persons = persons;
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+    }
+
+    @Override
+    public PersonViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item, viewGroup, false);
+        PersonViewHolder pvh = new PersonViewHolder(v);
+        return pvh;
+    }
+
+    @Override
+    public void onBindViewHolder(PersonViewHolder personViewHolder, int i) {
+        personViewHolder.personName.setText(persons.get(i).name);
+        personViewHolder.personAge.setText(persons.get(i).age);
+    }
+
+    @Override
+    public int getItemCount() {
+        return persons.size();
+    }
+}
